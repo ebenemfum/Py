dbe8dbdfb72d83eede6f0e02bce3942d70603982
ebenemfum <ebenezer.amoah@ashesi.edu.gh>
2021-03-27 20:27:21 +0000

    Initial commit

---
+++ TETZ.py
@@ -0,0 +1,27 @@
+# game = (input("Please enter your name"))
+# print(game)
+# pra = eval(input("Please enter an expression"))
+# print(pra + 1)
+# Swapping values of variables
+# Initial Value X= 2 Y=4
+# X = 2
+# Y = 4
+
+
+Temp = 2
+X = Y
+Y = Temp
+print(X, Y)
+# Simplified
+ X, Y = Y, X
+ print(X, Y)
+ def main():
+     print('This program computes the average of two exam scores. ')
+    print("The average of the scores is: ", average)
+
+
+# main()
+
+#anger = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, ]
+for x in anger:
+    print(x*x)
